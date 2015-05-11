a901ea7e6b09a2aa9469bd315390d35042af69cd
websioux <palazzi@gmail.com>
2015-05-11 09:02:44 +0200

    added pdf support

--- _init.php
+++ _init.php
@@ -1,7 +1,18 @@
 <?php 
 require_once('config_PRO_talk.php');
 require_once(dirname(__FILE__).'/talk/connect.php');
-cyberMailing_connect::clicTracking(); 
+cyberMailing_connect::clicTracking();
+list($sFname, $sExt) = explode('.',$_SERVER['SCRIPT_NAME']);
+if($sExt=='pdf') {
+	$file = $_SERVER['SCRIPT_FILENAME'];
+	$filename = $sFname .'.pdf'; 
+header('Content-type: application/pdf');
+header('Content-Disposition: inline; filename="' . $filename . '"');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: ' . filesize($file));
+header('Accept-Ranges: bytes');
+@readfile($file);
+}
 
 /*
 
